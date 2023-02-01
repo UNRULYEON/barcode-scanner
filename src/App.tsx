8dfcc0ea7012325fa52b497b5c43593b9992eea0
Amar Kisoensingh <amar@kisoensingh.sh>
2023-02-01 22:17:45 +0100

    Add browser info

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { ReactNode, useEffect, useState } from "react";
-import Button from "./Button";
+import {
+  ReactNode,
+  useEffect,
+  useState,
+  ButtonHTMLAttributes,
+  FC,
+} from "react";
+import uaParser from "ua-parser-js";
 
-const Row = ({ children }: { children: ReactNode }) => (
-  <span className="flex flex-row items-center text-lg font-bold">
-    {children}
-  </span>
-);
-
-const Checkmark = () => (
-  <span className="material-symbols-rounded mr-2 text-green-500">
-    check_circle
-  </span>
-);
+const parser = new uaParser();
+const { name: browserName, version: browserVersion } = parser.getBrowser();
 
 function App() {
   const [hidCompatible] = useState("hid" in navigator);
@@ -24,7 +21,10 @@ function App() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-stone-800 text-white">
       <div className="flex h-full max-h-[400px] w-full max-w-[300px] flex-col gap-4 rounded-xl  bg-stone-700 p-4">
-        <Row>{hidCompatible && <Checkmark />}HID compatible</Row>
+        <span className="text-center text-sm">
+          {browserName} {browserVersion}
+        </span>
+        <Row>{hidCompatible && <Checkmark />} HID compatible</Row>
         <Button onClick={requestDevices}>Request Devices</Button>
       </div>
     </div>
@@ -32,3 +32,33 @@ function App() {
 }
 
 export default App;
+
+type ButtonProps = {
+  children: React.ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <button
+      className="rounded-full bg-stone-800 p-3 font-bold transition-all hover:bg-stone-900"
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+const Row = ({ children }: { children: ReactNode }) => (
+  <span
+    className="grid items-center gap-2 text-lg font-bold"
+    style={{
+      gridTemplateColumns: "24px 1fr",
+    }}
+  >
+    {children}
+  </span>
+);
+
+const Checkmark = () => (
+  <span className="material-symbols-rounded text-green-500">check_circle</span>
+);
